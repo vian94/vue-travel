c6ea0ea4ea307a2c5adca4704d316543398a75a9
vian94 <46313696+vian94@users.noreply.github.com>
2019-08-12 21:13:07 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// fastClick，解决移动端300毫秒点击延迟（原本默认双击缩放造成）
+// 绑定到document.body上
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
